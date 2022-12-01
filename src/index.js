ee126680667218503b02083643a8785eb1f9461f
otienoonyango2000 <otienoonyango2000@gmail.com>
2022-12-01 18:52:46 +0300

    tasklister mini completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildTaskListerLite(e.target.new_task_description.value)
+    form.reset
+  })
+})
+
+function buildTaskListerLite(createtask) {
+  let p = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = createtask
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+  
+  
+
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
